dbdfa9aeb6b8c06b5b1ec052da648bfb1984499d
Piyush <piyush41y08h@gmail.com>
2020-11-29 18:55:07 +0530

    Initial with v1 complete

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ["./src/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff4c68",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
